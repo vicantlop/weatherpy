2102e704fc30b4fab11da65fc0805b44946e8a6c
vicantlop <vicantlop@gmail.com>
2023-05-16 09:15:21 -0700

    issue1

--- app.py
+++ app.py
@@ -49,3 +49,4 @@ threeday.forecast()
 # current.temp_f, .condition.text
 # forecast.forecastday[0], forecast.forecastday[0].maxtemp_f, forecast.forecastday[0].mintemp_f
 # forecast.forecastday[1], forecast.forecastday[1].maxtemp_f, forecast.forecastday[1].mintemp_f, forecast.forecastday[1].condition.text
+#changes
